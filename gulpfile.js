a9c646db9702a22294f907e58375d942c18ebf6c
Gökhan Taşkan <gokhan.frontfeed@gmail.com>
2018-12-23 02:40:39 +0300

    nano +

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,32 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const concat = require('gulp-concat');
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
   return gulp.src('./src/assets/styles/sass/main.scss')
     .pipe(sass({
-      outputStyle: ':compact'
+      outputStyle: 'compact'
     }).on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions']
     }))
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('./src/assets/styles/css'));
+});
+
+gulp.task('sass:nano', function () {
+  return gulp.src('./src/assets/styles/sass/main.scss')
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(cssnano())
+    .pipe(concat('main.nano.css'))
     .pipe(gulp.dest('./src/assets/styles/css'));
 });
 
